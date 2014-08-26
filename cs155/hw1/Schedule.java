571200d96859fc8344d18a4ea9e00543493a590a
Tim Stullich <timstullich@gmail.com>
2014-08-25 23:12:17 -0700

    Some painful refactoring was done

--- cs155/hw1/Schedule.java
+++ cs155/hw1/Schedule.java
@@ -1,91 +1,80 @@
 import java.util.Arrays;
 
 /**
-    Represents a schedule of tasks for the Two Processor Scheduling problem
-    @author Jeff Smith
-    @version for CS 155, Fall 2014, SJSU
+ *  Represents a schedule of tasks for the Two Processor Scheduling problem
+ *  @author Jeff Smith
+ *  @version for CS 155, Fall 2014, SJSU
  */
 
 public class Schedule
 {
     // the schedule, from first task to last
-    //   if entry i is true, then task i is scheduled for the first processor,
-    //   otherwise it's scheduled for the second processor
-    
+    // if entry i is true, then task i is scheduled for the first processor,
+    // otherwise it's scheduled for the second processor
+
     private boolean[] isForFirstProcessor;
 
     // total times of the tasks scheduled for processors 1 and 2 respectively
-    
     private int totalTime1 = 0;
     private int totalTime2 = 0;
 
-    
-    /**
-         Constructs an empty Schedule for n tasks
-     */
-    
-    public Schedule(int n)
-    {
-       isForFirstProcessor = new boolean[n]; 
+    // Constructs an empty Schedule for n tasks
+    public Schedule(int n) {
+       isForFirstProcessor = new boolean[n];
        totalTime1 = 0;
        totalTime2 = 0;
     }
 
     /**
-         Constructs an Schedule with a given assignment of tasks
-           and total times used by both processors
-         @param b the assignment of tasks
-         @param t1 the total time of the tasks assigned to processor 1
-         @param t1 the total time of the tasks assigned to processor 2
+     * Constructs a Schedule with a given assignment of tasks
+     * and total times used by both processors
+     * @param b the assignment of tasks
+     * @param t1 the total time of the tasks assigned to processor 1
+     * @param t1 the total time of the tasks assigned to processor 2
      */
-    
-    public Schedule(boolean[] b, int t1, int t2)
-    {
-       isForFirstProcessor = Arrays.copyOf(b, b.length); 
+    public Schedule(boolean[] b, int t1, int t2) {
+       isForFirstProcessor = Arrays.copyOf(b, b.length);
        totalTime1 = t1;
        totalTime2 = t2;
     }
 
-    
     /**
-         Gets the total time of the tasks assigned to processor 1
-         @return the total time of the tasks assigned to processor 1
+     * Gets the total time of the tasks assigned to processor 1
+     * @return the total time of the tasks assigned to processor 1
      */
-    
-    public int getTotalTime1()   { return totalTime1; }
+    public int getTotalTime1() {
+        return totalTime1;
+    }
 
-    
     /**
-         Gets the total time of the tasks assigned to processor 2
-         @return the total time of the tasks assigned to processor 2
+     * Gets the total time of the tasks assigned to processor 2
+     * @return the total time of the tasks assigned to processor 2
      */
-    
-    public int getTotalTime2()   { return totalTime2; }
+    public int getTotalTime2() {
+        return totalTime2;
+    }
 
-    
     /**
-         Gets the schedule of tasks
-         @return a copy of the schedule of tasks
+     * Gets the schedule of tasks
+     * @return a copy of the schedule of tasks
      */
-    
-    public boolean[] getSked()   { return Arrays.copyOf(isForFirstProcessor, isForFirstProcessor.length);  }
+    public boolean[] getSked() {
+        return Arrays.copyOf(isForFirstProcessor, isForFirstProcessor.length);
+    }
 
-    
     /**
-         Finds the time required by the schedule
-         @return the larger of the time required by processor 1 and the time required by processor 2
+     * Finds the time required by the schedule
+     * @return the larger of the time required by processor 1 and the time required by processor 2
      */
-    
-    public int getTotalTime()    { return Math.max(totalTime1, totalTime2); }
-                                      
-    
+    public int getTotalTime() {
+        return Math.max(totalTime1, totalTime2);
+    }
+
     /**
-        Gets a string version of the schedule
-        @return a string version of the schedule
+     * Gets a string version of the schedule
+     * @return a string version of the schedule
      */
-    
-    public String toString()
-    {
+    public String toString() {
         return Arrays.toString(isForFirstProcessor) + " " + totalTime1 + ":" + totalTime2;
     }
 }
--- cs155/hw1/TwoProcessorScheduler.java
+++ cs155/hw1/TwoProcessorScheduler.java
@@ -2,92 +2,82 @@ import java.util.Random;
 import java.util.Arrays;
 
 /**
-    Provides exact and greedy approximate solution algorithms for the Two Processor Scheduling problem.
-    The greedy algorithms work by considering the tasks in some order and assigning the next task
-      to the processor with the smallest load so far.
-    @author Jeff Smith
-    @version for CS 155, Fall 2014, SJSU
+ * Provides exact and greedy approximate solution algorithms for the Two Processor Scheduling problem.
+ * The greedy algorithms work by considering the tasks in some order and assigning the next task
+ * to the processor with the smallest load so far.
+ * @author Jeff Smith
+ * @version for CS 155, Fall 2014, SJSU
  */
-public class TwoProcessorScheduler
-{
-
+public class TwoProcessorScheduler {
     private int[] weight;                      // the sequence of task lengths
     public int n;                              // number of tasks
     public static final int MAX_LENGTH = 100;  // maximum length of a task
-    
-    
+
     /**
-        Randomly constructs an instance of the Two Processor Scheduling problem
-          where the task times are chosen uniformly and randomly from 1 through
-          MAX_LENGTH
-        @param n the number of tasks
-        @param r a random object for constructing the task times
-        @param seed a seed for the random object, to guarantee repeatability
+     * Randomly constructs an instance of the Two Processor Scheduling problem
+     * where the task times are chosen uniformly and randomly from 1 through
+     * MAX_LENGTH
+     * @param n the number of tasks
+     * @param r a random object for constructing the task times
+     * @param seed a seed for the random object, to guarantee repeatability
      */
-    
     public TwoProcessorScheduler(int n, Random r, int seed)  {
         this.n = n;
         weight = new int[n];
         r = new Random(seed);
-        for (int i=0; i<n; i++)
-          weight[i] = 1 + r.nextInt(MAX_LENGTH);
-        
+        for (int i = 0; i < n; i++) {
+            weight[i] = 1 + r.nextInt(MAX_LENGTH);
+        }
     }
 
-    
-     /**
-        Constructs an instance of the Two Processor Scheduling problem
-          with a given
-        @param w the sequence of task times, which is not copied
-        @param r a random object for constructing the task times
-        @param seed a seed for the random object, to guarantee repeatability
+    /**
+     * Constructs an instance of the Two Processor Scheduling problem
+     * with a given
+     * @param w the sequence of task times, which is not copied
+     * @param r a random object for constructing the task times
+     * @param seed a seed for the random object, to guarantee repeatability
      */
-    
-    public TwoProcessorScheduler(int[] w)                    {
+    public TwoProcessorScheduler(int[] w) {
         this.n = w.length;
         weight = w;
     }
 
+    /**
+     * Constructs an exact solution to the Two Processor Scheduling problem
+     * by calling a helper method that considers both an assignment to processor 1
+     * and procesor 2, continuing both solutions are recursively, and returning
+     * the better of the two final solutions.
+     * @return the resulting schedule
+     */
+    public Schedule exactBacktracking() {
+        boolean[] result = new boolean[n];
+        return exactRecursive(n, 0, 0, result);
+    }
 
     /**
-        Constructs an exact solution to the Two Processor Scheduling problem
-          by calling a helper method that considers both an assignment to processor 1
-          and procesor 2, continuing both solutions are recursively, and returning
-          the better of the two final solutions.
-        @return the resulting schedule
+     * Continues a partial solution to the Two Processor Scheduling problem by
+     * considering both an assignment to processor 1
+     * and procesor 2, continuing both solutions are recursively, and returning
+     * the better of the two final solutions.
+     * @param n number of tasks remaining to be considered
+     * @param total1 total time taken so for by processor 1
+     * @param total2 total time taken so for by processor 2
+     * @param result the partial schedule, in the format used by the Schedule class
+     * @return the resulting schedule
      */
-    public Schedule exactBacktracking()                       {
-        boolean[] result = new boolean[n];
-        return exactRecursive(n,0,0, result);                  }
-        
-        
-    /**    
-        Continues a partial solution to the Two Processor Scheduling problem by
-          considering both an assignment to processor 1
-          and procesor 2, continuing both solutions are recursively, and returning
-          the better of the two final solutions.
-        @param n number of tasks remaining to be considered
-        @param total1 total time taken so for by processor 1
-        @param total2 total time taken so for by processor 2
-        @param result the partial schedule, in the format used by the Schedule class
-        @return the resulting schedule
-     */
-    
-    private Schedule exactRecursive(int n, int total1, int total2, boolean[] result)  {    
-      if (n==0)
-        return new Schedule(result, total1, total2);
-      else                                              {  
-        result[n-1] = true;
-        Schedule ifYes = exactRecursive(n-1, total1+weight[n-1], total2, result);
-        result[n-1] = false;
-        Schedule ifNo = exactRecursive(n-1, total1, total2+weight[n-1], result);
-        int error1 = Math.abs(ifYes.getTotalTime1()-ifYes.getTotalTime2());
-        int error2 = Math.abs(ifNo.getTotalTime1()-ifNo.getTotalTime2());
-        return error1<=error2 ? ifYes : ifNo;    
-                                                               }}
-    
-    
-    
+    private Schedule exactRecursive(int n, int total1, int total2, boolean[] result)  {
+        if (n == 0) {
+            return new Schedule(result, total1, total2);
+        }
+        else {
+            result[n-1] = true;
+            Schedule ifYes = exactRecursive(n - 1, total1 + weight[n - 1], total2, result);
+            result[n-1] = false;
+            Schedule ifNo = exactRecursive(n - 1, total1, total2 + weight[n - 1], result);
+            int error1 = Math.abs(ifYes.getTotalTime1() - ifYes.getTotalTime2());
+            int error2 = Math.abs(ifNo.getTotalTime1() - ifNo.getTotalTime2());
 
-       
+            return error1 <= error2 ? ifYes : ifNo;
+        }
+    }
 }
