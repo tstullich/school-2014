cb2f13507a3ecf04bc7d7814b899af98729e1918
Tim Stullich <timstullich@gmail.com>
2015-02-02 11:01:21 -0800

    Added some files for lab 1 in CS154

---
+++ spring2015/cs154/Operations.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.util.function.*;
+
+public class Operations {
+    public static <T> Set<T> intersect(Set<T> a, Set<T> b) {
+        Set<T> c = new HashSet<T>();
+        for (T ele : a) {
+            if (b.contains(ele)) {
+                c.add(ele);
+            }
+        }
+        return c;
+    }
+
+    public static <T> Set<T> union(Set<T> a, Set<T> b) {
+        Set<T> c = new HashSet<T>();
+        for (T ele : a) {
+            c.add(ele);
+        }
+        for (T ele : b) {
+            c.add(ele);
+        }
+        return c;
+    }
+
+    public static <T> Set<T> diff(Set<T> a, Set<T> b) {
+        Set<T> c = new HashSet<T>();
+        for (T ele : a) {
+            if (!b.contains(ele)) {
+                c.add(ele);
+            }
+        }
+        return c;
+
+    }
+
+    public static <T> boolean subset(Set<T> a, Set<T> b) {
+        for (T ele : a) {
+            if (!b.contains(ele)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static <T> Set<T> filter(Set<T> a, Predicate<T> f) {
+        return a;
+    }
+
+    public static <T> Set<T> map(Set<T> a, UnaryOperator<T> f) {
+        return a;
+    }
+}
---
+++ spring2015/cs154/Tester.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.function.*;
+
+public class Tester {
+
+public static void main(String args[]) {
+    Set<Integer> a = new HashSet<Integer>();
+    
+    // a = first 20 multiples of 3
+    for(int i = 0; i < 20; i++) a.add(i * 3);
+        Set<Integer>  b = Operations.filter(a,(Integer x)->x%2==0);
+        Set<Integer> c = Operations.map(b, (x)->2 * x);
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+    }
+}
