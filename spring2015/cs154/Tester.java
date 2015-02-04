9040dd38e400eb441e5e100f5df085c0061c54ac
Tim Stullich <timstullich@gmail.com>
2015-02-04 11:10:49 -0800

    Did some work on Tester for CS154 homework 1

--- spring2015/cs154/Tester.java
+++ spring2015/cs154/Tester.java
@@ -10,10 +10,22 @@ public static void main(String args[]) {
     for(int i = 0; i < 20; i++) {
         a.add(i * 3);
     }
+
     Set<Integer> b = Operations.filter(a, (Integer x) -> x % 2 == 0);
     Set<Integer> c = Operations.map(b, (x) -> 2 * x);
     System.out.println("a = " + a);
     System.out.println("b = " + b);
     System.out.println("c = " + c);
+
+    Set<Integer> d = new HashSet<Integer>();
+    d.add(1);
+    d.add(2);
+    d.add(3);
+    d.add(4);
+    d.add(5);
+    Set<Set<Integer>> pset = Operations.power(d);
+    for (Set<Integer> s : pset) {
+        System.out.println(s);
+    }
     }
 }
