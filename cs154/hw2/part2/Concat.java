bd536bdc0b52cb06d5c099f350491c26f38f271a
Tim Stullich <timstullich@gmail.com>
2015-02-27 20:35:35 -0800

    Finished CS 154 Lab 3

--- cs154/hw2/part2/Concat.java
+++ cs154/hw2/part2/Concat.java
@@ -7,18 +7,24 @@ public class Concat implements RegEx {
     }
 
     public boolean matches(String str) {
-        int index = 0;
-        while (index < str.length() - 1 &&
-               first.matches(str.substring(index, str.length()))) {
-            index++;
+        if (str.length() == 0) {
+            return true;
         }
-        if (index == 0) {
-            return false;
+
+        for (int i = 1; i <= str.length(); i++) {
+            String sub = str.substring(0, i);
+            if (!first.matches(sub)) {
+                return matches(str.substring(i, str.length()));
+            }
         }
-        while (index < str.length() - 1 &&
-               second.matches(str.substring(index, str.length()))) {
-            index++;
+
+        for (int i = 1; i <= str.length(); i++) {
+            String sub = str.substring(0, i);
+            if (!second.matches(sub)) {
+                return matches(str.substring(i, str.length()));
+            }
         }
-        return index == str.length() - 1;
+
+        return false;
     }
 }
